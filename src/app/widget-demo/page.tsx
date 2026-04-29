a9b65233f325fc9440904f44a52eb1b1c5a74a0b
Klemens Kaindl <klemens.kaindl@gmail.com>
2026-04-29 17:53:05 +0200

    fix: force-dynamic widget-demo to avoid build-time DB query

--- src/app/widget-demo/page.tsx
+++ src/app/widget-demo/page.tsx
@@ -2,6 +2,8 @@ import { db } from "@/lib/db";
 import Script from "next/script";
 import Link from "next/link";
 
+export const dynamic = "force-dynamic";
+
 export default async function WidgetDemo() {
   const tenant = await db.tenant.findFirst({ where: { slug: "greinsfurth" } });
 
